8c823a80ff5cf22731202378b1ce46e1ebc5e5cc
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2022-11-17 10:01:27 +0800

    adding typed react

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Typed from "react-typed";
 
 const Hero = () => {
   return (
@@ -8,8 +9,9 @@ const Hero = () => {
         <h1>From seed to bloom.</h1>
         <div>
           <p>Start with the Fundamentals of </p>
-
+          <Typed />
           <p>Improving by using </p>
+          <Typed />
         </div>
         <p>I currently studying programming in almost 2months</p>
         <button>Info</button>
