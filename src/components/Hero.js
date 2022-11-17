e6c7abb77b9225f75b18bed197de11266c9d131c
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2022-11-17 12:40:22 +0800

    Adding hero with typed effect

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,12 +4,17 @@ import Typed from "react-typed";
 const Hero = () => {
   return (
     <div className="text-white">
-      <div>
-        <p>Progress in Programming</p>
-        <h1>From seed to bloom.</h1>
+      <div className="max-w-[800px] mt-[1px] w-full h-screen mx-auto text-center felx flex-col justify-center">
+        <p className="text-[#00df9a] font-bold p-2">PROGRESS IN PROGRAMMING</p>
+        <h2 className="text-4xl font-bold sm:text-6xl sm:py-4 md:text-7xl md:py-6">
+          From seed to bloom
+        </h2>
         <div>
-          <p>Start with the Fundamentals of </p>
+          <p className="text-xl font-bold p-4 sm:text-2xl md:text-4xl">
+            Start with the Fundamentals of{" "}
+          </p>
           <Typed
+            className="text-gray-500 font-mono text-3xl font-bold pl-2 sm:text-4xl md:text-5xl md:pl-4"
             strings={["HTML", "CSS", "JAVASCRIPT"]}
             typeSpeed={60}
             backSpeed={40}
@@ -17,16 +22,23 @@ const Hero = () => {
           />
         </div>
         <div>
-          <p>Improving by using </p>
+          <p className="text-xl font-bold py-4 sm:text-2xl md:text-4xl">
+            Improving by using{" "}
+          </p>
           <Typed
+            className="text-gray-500 font-mono text-3xl font-bold pl-2 sm:text-4xl md:text-5xl md:pl-4"
             strings={["REACT", "TAILWIND-CSS"]}
             typeSpeed={60}
             backSpeed={40}
             loop
           />
         </div>
-        <p>I currently studying programming in almost 2months</p>
-        <button>Info</button>
+        <p className=" text-sm font-light p-4 md:text-4xl">
+          I currently studying programming in almost 2 months
+        </p>
+        <button className="bg-[#00df9a] w-[200px] rounded-md font-medium my-2 mx-auto py-3 text-black font-5xl sm:text-2xl md:text-3xl hover:text-white hover:scale-105 duration-500">
+          Info
+        </button>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,7 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-      screens: {
-        ps: "375px",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
