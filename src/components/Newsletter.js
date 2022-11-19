6b97a45443593081fbd8b1007db5e064f04ece6e
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2022-11-19 12:03:42 +0800

    adding outline color in newsletter section input text

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -16,7 +16,7 @@ const Newsletter = () => {
               type="email"
               name="email"
               placeholder="Type your email"
-              className="p-3 flex w-full rounded-md text-black"
+              className="p-3 flex w-full rounded-md text-black focus:ring focus:ring-[#00df9a] focus:outline-none"
             />
             <button className="bg-[#00df9a] text-black w-[200px] rounded-md font-medium ml-4 my-4 px-6 py-3 hover:bg-[#03faac] hover:scale-105 duration-300">
               Notify Me
